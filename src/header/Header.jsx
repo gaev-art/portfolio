ce2f9af1ac3c0f19805cae072ae6d504370bac4b
gaev-art <forsakensr@gmail.com>
2020-08-23 18:56:53 +0300

    -deleted the .idea folder from  the repository git -added to the Header component

---
+++ src/header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import style from './Header.module.css';
+import NavMenu from "./NavMenu/NavMenu";
+
+const Header = () => (
+    <div className={style.header}>
+        <div className={style.container}>
+            <NavMenu/>
+        </div>
+    </div>
+);
+
+export default Header;

20aa6f9aecdd2b780edbdc884ad2a830f80d53cb
gaev-art <forsakensr@gmail.com>
2023-03-19 22:29:49 +0100

    feat: setup routing

--- src/index.js
+++ src/index.js
@@ -1,17 +1,45 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Navigate,
+  NavLink,
+  Outlet,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 
 import "./i18n";
 import "./index.css";
-import { App } from "./App";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route
+      path="/"
+      element={
+        <Suspense fallback={<div>Loading...</div>}>
+          <div className=" flex h-[70px] justify-center items-center gap-10 w-full">
+            <NavLink to="/aboutMe">aboutMe</NavLink>
+            <NavLink to="/projects">projects</NavLink>
+          </div>
+          <Outlet />
+        </Suspense>
+      }
+    >
+      <Route path="/" element={<div>Main</div>} />
+      <Route path="/aboutMe" element={<div>AboutMe</div>} />
+      <Route path="/projects" element={<div>Projects</div>} />
+      <Route path="*" element={<Navigate to="/" />} />
+    </Route>
+  )
+);
+
 root.render(
   <React.StrictMode>
-    <h1 className="text-3xl font-bold underline">
-      <App />
-    </h1>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );

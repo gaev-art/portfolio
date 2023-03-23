52838fcb9b2596f442b77cd0f720015997fc4457
gaev-art <forsakensr@gmail.com>
2023-03-23 22:20:17 +0100

    feat: add social media icons

--- src/shared/components/Header.jsx
+++ src/shared/components/Header.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import { useTranslation } from "react-i18next";
+import { Icon } from "./Icon";
+import { icons } from "../shared.constants";
 
 export const Header = () => {
   const { t, i18n } = useTranslation();
@@ -19,12 +21,16 @@ export const Header = () => {
       style={{ boxShadow: "0 0 5px rgba(255, 255, 255, 0.5)" }}
       className="flex h-[70px] justify-between items-center gap-10 w-full"
     >
-      <div></div>
+      <div className="flex gap-5 mx-5">
+        {icons.map((icon) => (
+          <Icon key={icon.href} href={icon.href} icon={icon.icon} />
+        ))}
+      </div>
       <div className="flex gap-10">
         <NavLink
           to="/aboutMe"
           className={({ isActive }) =>
-            isActive ? "text-white" : "text-[#4a4a4a]"
+            isActive ? "text-white" : "text-[#4a4a4a] hover:text-white"
           }
         >
           {t("about_me")}
@@ -32,17 +38,19 @@ export const Header = () => {
         <NavLink
           to="/projects"
           className={({ isActive }) =>
-            isActive ? "text-white" : "text-[#4a4a4a]"
+            isActive ? "text-white" : "text-[#4a4a4a] hover:text-white"
           }
         >
           {t("my_projects")}
         </NavLink>
       </div>
-      <div className="relative mx-5">
+      <div className="flex justify-end relative mx-5">
         <div
           onClick={openMenu}
-          className="flex w-[60px] h-[28px] justify-center rounded-[10px] items-center"
-          style={{ boxShadow: "0 0 2px rgba(255, 255, 255, 0.5)" }}
+          className="flex w-[60px] h-[28px] justify-center rounded-[10px] items-center p-1"
+          style={{
+            boxShadow: !isActive ? "0 0 2px rgba(255, 255, 255, 0.5)" : null,
+          }}
         >
           <span className="uppercase font-bold">{language}</span>
         </div>
@@ -51,7 +59,7 @@ export const Header = () => {
           style={{ boxShadow: "0 0 2px rgba(255, 255, 255, 0.5)" }}
           className={
             isActive
-              ? "flex absolute right-0 top-0 bg-[#000000] w-[60px] justify-center rounded-[10px] items-center"
+              ? "flex absolute right-0 top-0 bg-[#000000] w-[60px] justify-center rounded-[10px] items-center p-1"
               : "hidden"
           }
         >
@@ -65,7 +73,9 @@ export const Header = () => {
                   setIsActive(false);
                 }}
               >
-                <span className="uppercase font-bold">{language}</span>
+                <span className="uppercase font-bold text-[#4a4a4a] hover:text-white">
+                  {language}
+                </span>
               </li>
             ))}
           </ul>
